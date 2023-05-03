7ede03ff9c2048f02cf5f76225b070b49048072d
Darren Barklie <darrenbarklie@gmail.com>
2023-05-03 23:02:41 +0100

    Iterator and collect

---
+++ rust-typescript/src/index.ts
@@ -0,0 +1,5 @@
+const list = [1, 2, 3];
+const addList = list.map((item) => item + 1);
+
+console.log(list);
+console.log(addList);
--- rust-typescript/src/main.rs
+++ rust-typescript/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // Iterate over a Vector and return modified values
+    // let list: Vec<_> = vec![1, 2, 3].iter().map(|i| return i + 1).collect();
+    // println!("Result: {:?}", list);
+
+    // Manually demostrate the .collect() method
+    let data = vec![1, 2, 3];
+    let mut list = data.iter().map(|x| x + 1);
+    let mut new_vector = vec![];
+    while let Some(x) = list.next() {
+        new_vector.push(x)
+    }
+    println!("Result: {:?}", new_vector);
 }
