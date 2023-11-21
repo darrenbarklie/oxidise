fce8bcb3e1c7d7b347e83a387896f84eba7372b6
Darren Barklie <darrenbarklie@gmail.com>
2023-11-21 23:18:46 +0000

    reading and writing files

--- programming-rust/02-tour-of-rust/05-filesystem-cmd-line/src/main.rs
+++ programming-rust/02-tour-of-rust/05-filesystem-cmd-line/src/main.rs
@@ -1,8 +1,25 @@
-
+use std::fs;
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+    
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(err) => {
+            eprintln!("{} failed to read from file '{}': {:?}",
+                      "Error:".red().bold(), args.filename, err);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {},
+        Err(err) => {
+            eprintln!("{} failed to write to file '{}': {:?}",
+                      "Error:".red().bold(), args.filename, err);
+            std::process::exit(1);
+        }
+    }
 }
 
 use text_colorizer::*;
