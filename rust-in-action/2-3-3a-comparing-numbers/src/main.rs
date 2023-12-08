971ffec96047c3c30657d71d19d97cc2f217492c
Darren Barklie <darrenbarklie@gmail.com>
2023-12-08 21:25:43 +0000

    Comparing numbers

--- rust-in-action/2-3-3a-comparing-numbers/src/main.rs
+++ rust-in-action/2-3-3a-comparing-numbers/src/main.rs
@@ -16,5 +16,35 @@ fn main() {
         println!("Ten is less than one thousand.");
     }
 
-    println!("Hello, world!");
+    // error: assertion failed
+    // assert!(0.1 + 0.2 == 0.3);
+   
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("  0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("        0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    
+    println!("abc (f64)");
+    println!("  0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("        0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    // expect error: assertation failed
+    // assert!(xyz.0 + xyz.1 == xyz.2);
+
+    let result: f32 = 0.1 + 0.1;
+    let desired: f32 = 0.2;
+    let absolute_difference = (desired - result).abs();
+    assert!(absolute_difference <= f32::EPSILON);
+
+    // expect_error: assertion `left == right` failed
+    // let x = (-42.0_f32).sqrt();
+    // assert_eq!(x, x);
+
+    println!("Done!");
 }
