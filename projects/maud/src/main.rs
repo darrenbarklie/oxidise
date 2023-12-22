c2302a0a5c409d50bfd66e632a1bb07771dcfc32
Darren Barklie <darrenbarklie@gmail.com>
2023-12-22 01:26:37 +0000

    Maud escape sequencing

--- projects/maud/src/main.rs
+++ projects/maud/src/main.rs
@@ -1,4 +1,5 @@
 use maud::html;
+use maud::PreEscaped;
 
 fn main() {
     let name = "Darren";
@@ -8,5 +9,41 @@ fn main() {
 
     println!("{}", markup.into_string());
 
+
+    let markup_esc_char = html! {
+        "\"Oatmeal, are you crazy?\" he asked.\n"
+        (PreEscaped("\"Oatmeal, are you crazy?\" he asked."))
+    };
+
+    println!("{}", markup_esc_char.into_string());
+
+    let markup_esc_block = html! {
+        pre {
+            r#"
+                Rocks, these are my rocks.
+                Sediments make me sedimental.
+                Smooth and round,
+                Asleep in the ground.
+                Shades of brown
+                And gray.
+            "#
+        }
+    };
+
+    println!("{}", markup_esc_block.into_string());
+
+    let document_object_model = dom();
+
+    println!("{:?}", document_object_model);
+
     println!("Done!");
+}
+
+use maud::DOCTYPE;
+
+fn dom () -> PreEscaped<String> {
+    html! {
+        (DOCTYPE)
+        p { "Hello world!" }
+    }
 }
