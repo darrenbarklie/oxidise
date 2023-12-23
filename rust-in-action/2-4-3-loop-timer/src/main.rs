5fe3ac1f7a00ceafae5b9a14fc54b216fb3b2ffc
Darren Barklie <darrenbarklie@gmail.com>
2023-12-23 23:55:27 +0000

    Loop timer

---
+++ rust-in-action/2-4-3-loop-timer/src/main.rs
@@ -0,0 +1,20 @@
+use std::time::{Duration, Instant};
+
+fn main() {
+    let mut count = 0;
+    let time_limit = Duration::new(1,0);
+    let start = Instant::now();
+
+    while (Instant::now() - start) < time_limit {
+        count += 1;
+    }
+
+    println!("{}", count);
+
+    println!("Done!");
+}
+
+// AVOID
+// while true {
+//     println!("Are we there yet?");
+// }
