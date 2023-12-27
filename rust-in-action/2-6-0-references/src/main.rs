093be3f2f7e09e0ec58462cbbc593e15e0f06feb
darrenbarklie <darrenbarklie@gmail.com>
2023-12-27 21:07:47 +0000

    Reference, dereference

--- rust-in-action/2-6-0-references/src/main.rs
+++ rust-in-action/2-6-0-references/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     let a = 42;
-    let r = &a;
 
-    let b = a + *r;
+    let r = &a;         // referencing
+
+    let b = a + *r;     // dereferencing
 
     println!("a + a = {}", b);
+
+    let needle = 21147;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+
+    for item in &haystack {
+        if *item == needle {
+            println!("{}", item);
+        }
+    }
 
     println!("Done!");
 }
